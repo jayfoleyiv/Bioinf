05c7bd24c0a86c83b65b44176a1cfeba0b9e8152
Remm97 <eldabaghr@student.wpunj.edu>
2017-08-03 20:52:36 -0400

    trial 8

--- Pearsons_CC/Python_Code/Test.py
+++ Pearsons_CC/Python_Code/Test.py
@@ -277,7 +277,7 @@ chart1 = workbook.add_chart({'type': 'column'})
 
 # Configure the first series.
 chart1.add_series({
-    'categories': '=Sheet1!$A$2:$A$41',
+    'categories': '=Sheet1!$A$1:$A$41',
     'values': '=Sheet1!$B$1:$B$41',
 })
 
