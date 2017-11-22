a68b89a1d8b74401eb519e79f3d913867c54f486
Remm97 <eldabaghr@student.wpunj.edu>
2017-11-22 13:35:05 -0500

    weas

--- Pearsons_CC/Python_Code/Test_Beta.py
+++ Pearsons_CC/Python_Code/Test_Beta.py
@@ -355,8 +355,8 @@ for thing in (genes):
 chart2 = workbook.add_chart({'type': 'column'})
 
 chart2.add_series({
-    'categories' : '=Randomness_Check!$A$1:$A$79',
-    'values' : '=Randomness_Check!$B$1:$B$79',
+    'categories' : '=Randomness_Check!$A$1:$A$172',
+    'values' : '=Randomness_Check!$B$1:$B$172',
 })
 
 chart2.set_title ({'name': 'Randomness Check'})
